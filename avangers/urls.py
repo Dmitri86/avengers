5c9d0cbcb4fa58b58926b6436c97d531eaaa607b
Dmitriy <zhirkod@gmail.com>
2019-03-06 15:51:12 +0200

    Fix double folder avengers

--- avangers/urls.py
+++ avangers/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^thor/', include('thor.urls')),
-    url(r'^', include('avengers.urls')),
+    url(r'^', include('avg.urls')),
     url(r'^iron_man', include('iron_man.urls')),
     url(r'^captain_america', include('captain_america.urls')),
     url(r'^hulk', include('hulk.urls')),
