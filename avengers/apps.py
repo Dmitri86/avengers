17775e6237642929b6fef2c4710ee947d3067d46
Dmitriy <zhirkod@gmail.com>
2019-03-06 15:56:21 +0200

    delite avengers folders

--- avengers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AvengersConfig(AppConfig):
-    name = 'avengers'
--- avengers/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def show(request):
-    return render(request, "avengers/1.html")
-
-# Create your views here.
