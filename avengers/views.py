b12c33656d9baa25fa72158f5098fb284be3a9e4
Dmitriy <zhirkod@gmail.com>
2019-03-06 15:05:59 +0200

    Change main page

--- avengers/views.py
+++ avengers/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def show(request):
-    return HttpResponse("<img src=\"https://www.hdwallpapers.in/download/avengers_infinity_war_4k_8k-2560x1600.jpg\" alt=\"\" width=100% height=98.5%>")
+    return render(request, "avengers/1.html")
 
 # Create your views here.
